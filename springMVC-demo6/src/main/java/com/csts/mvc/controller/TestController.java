2c7f6aff0a2a916aacb0068e0635d4f6d97ce886
cstsncv <cstsncv@sina.com>
2021-12-09 16:41:40 +0800

    Interceptor

--- springMVC-demo6/src/main/java/com/csts/mvc/controller/TestController.java
+++ springMVC-demo6/src/main/java/com/csts/mvc/controller/TestController.java
@@ -1,9 +1,15 @@
 package com.csts.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
 
+    @RequestMapping("/**/testInterceptor")
+    public String testInterceptor(){
+
+        return "success";
+    }
 
 }
